6e31ba4da02d0de7597bb92274202a93c3d9b853
azamat98 <mr.azik98@mail.ru>
2019-04-20 23:21:43 +0500

    1st exercise

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+});
+Route::get('post/{id}', function ($id) {
+    return view('blog.post')->with('id',$id);
 });
